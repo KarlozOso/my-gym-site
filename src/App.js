4017313faddf4389894fe77c44c5f3af58dd8f10
KarlozOso <karlozoso90@gmail.com>
2023-08-17 04:45:52 -0600

    Plans and Reasonscomplete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs'
+import Reasons from './components/Reasons/Reasons';
+import Testimonials from './components/Testimonials/Testimonials';
 
 
 
@@ -9,6 +12,9 @@ function App() {
     <div className="App">
          <Hero />
       <Programs />
+      <Reasons />
+      <Plans />
+      <Testimonials />
     </div>
   );
 }
