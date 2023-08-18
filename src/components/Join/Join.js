bd2ff473d0a7fd23869a37add2c5edfde261ea37
KarlozOso <135464400+KarlozOso@users.noreply.github.com>
2023-08-18 05:04:20 +0000

    Ui complete

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import './Join.css';
+import emailjs from "@emailjs/browser";
+
 
 const Join = () => {
-  return (
-    <div className='Join'> </div>
-  )
-}
+  const form = useRef()
+const sendEmail = (e) => {
+    e.preventDefault();
 
-export default Join
+    emailjs.sendForm('service_j9xiiem', 'template_sgtw9jb', form.current, 'zK60Pt7Q8_KH6RxjF')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+}; 
+  return (
+    <div className='Join' id='join-us'>
+    <div className='left-j'>
+      <hr />
+    <div>
+    <span className='stroke-text'>READY TO</span>
+      <span>LEVEL UP</span>
+    </div>
+      <div>
+    <span>YOUR BODY</span>
+      <span className='stroke-text'>WITH US?</span>
+    </div>
+    </div>
+    <div className='right-j'>
+    <form ref={form} action='' className='email-container' onSubmit={sendEmail}>
+    <input type='email' name='user-email' placeholder='Enter your E-mail'></input>
+      <button className='btn btn-j'>JOIN NOW</button>
+    </form>
+    </div>
+    </div>
+  );
+};
+
+export default Join;
